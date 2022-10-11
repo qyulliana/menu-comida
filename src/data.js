1f47a2ac5aa6d82d0456e2afe0a705ed93fe1949
Yulliana Quintana <yullianaquintana@192.168.1.7>
2022-10-11 11:26:26 -0300

    menucomida

---
+++ src/data.js
@@ -0,0 +1,77 @@
+
+
+const menu = [
+    {
+        id: 1,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: 'https://www.allrecipes.com/thmb/Qr4gaBDK6mnfPGipzQW2nzKVKZA=/2000x2000/filters:no_upscale()/21014-Good-old-Fashioned-Pancakes-mfs_002-0e249c95678f446291ebc9408ae64c05.jpg',
+        desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+        id: 2,
+        title: 'diner double',
+        category: 'lunch',
+        price: 13.99,
+        img: 'https://babycocina.com/wp-content/uploads/2021/01/desayuno-americano.jpeg',
+        desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+        id: 3,
+        title: 'godzilla milkshake',
+        category: 'shakes',
+        price: 6.99,
+        img: 'https://img.freepik.com/premium-photo/strawberry-smoothie-milkshake-with-berries-mint-tall-glass-light-grey-background-summer-drink-shake-milkshake-refreshment-organic-concept-place-text_253362-10822.jpg',
+        desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+        id: 4,
+        title: 'country delight',
+        category: 'breakfast',
+        price: 20.99,
+        img: 'https://mejorconsalud.as.com/wp-content/uploads/2018/02/Desayuno-light.jpg',
+        desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+        id: 5,
+        title: 'egg attack',
+        category: 'lunch',
+        price: 22.99,
+        img: 'https://mejorconsalud.as.com/wp-content/uploads/2014/11/Consumir-un-buen-desayuno.jpg',
+        desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+        id: 6,
+        title: 'oreo dream',
+        category: 'shakes',
+        price: 18.99,
+        img: 'https://dulcesperu.com/wp-content/uploads/2020/01/milshake-oreo-receta.jpg',
+        desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+        id: 7,
+        title: 'bacon overflow',
+        category: 'breakfast',
+        price: 8.99,
+        img: 'https://www.tello.es/wp-content/uploads/2016/12/Blog_Tello_Recetas30.jpg',
+        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+        id: 8,
+        title: 'american classic',
+        category: 'lunch',
+        price: 12.99,
+        img: 'https://www.cardamomo.news/__export/1620489825570/sites/debate/img/2021/05/08/desayuno_americano_crop1620489781525.jpeg_1187729725.jpeg',
+        desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+        id: 9,
+        title: 'quarantine buddy',
+        category: 'shakes',
+        price: 16.99,
+        img: 'https://www.theepicureanmouse.com/wp-content/uploads/2022/05/strawberry-banana-milkshake-featured-pic.jpg',
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+];
+export default menu;
